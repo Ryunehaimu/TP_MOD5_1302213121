444e124e8fd47ea94f29c37edb43f86ef0e04ab0
Ryunehaimu <108001133+Ryunehaimu@users.noreply.github.com>
2023-03-20 00:33:37 +0700

    menambahkan class DataGeneric

---
+++ TP_MOD5_1302213121/TP_MOD5_1302213121/DataGeneric_1302213121.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD5_1302213121
+{
+    internal class DataGeneric_1302213121<T>
+    {
+        private T data;
+        public DataGeneric_1302213121(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData_1302213121()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
--- TP_MOD5_1302213121/TP_MOD5_1302213121/Program.cs
+++ TP_MOD5_1302213121/TP_MOD5_1302213121/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TP_MOD5_1302213121;
+Console.Write("Masukkan NIM: ");
+string nim = Console.ReadLine();
+DataGeneric_1302213121<string> data = new DataGeneric_1302213121<string>(nim);
+data.PrintData_1302213121();
